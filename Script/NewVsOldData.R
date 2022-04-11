cd72333b7803cbbbe169b9807e69ce29696dab4c
camillanovaglio <camilla.novaglio@gmail.com>
2022-04-11 15:10:03 +1000

    compare global effort with original csv files

--- Script/NewVsOldData.R
+++ Script/NewVsOldData.R
@@ -20,17 +20,76 @@ yannick_dir_new <- "/rd/gem/private/users/yannickr"
 yannick_dir_old <- "/rd/gem/private/DataYannick"
 
 # NEW
-
 effort_new<-read_csv(file.path(yannick_dir_new, "all_effort_aggregated.csv"))
 head(effort_new)
 
 # OLD 
-
 effort_old<-read_csv(file.path(yannick_dir_old, "TotalAggregatedFGroupLME.csv"))
-head(effort_old)
 effort_old<-effort_old[2:nrow(effort_old), 2:ncol(effort_old)]
 head(effort_old)
 
+# plot all effort 
+effort_mapped_new<-effort_new %>% 
+  group_by(Year, Sector) %>% 
+  summarise(effort = sum(NomActive, na.rm = TRUE)) %>% 
+  ungroup() %>% 
+  mutate(Version = "New")
+
+effort_mapped_old<-effort_old %>% 
+  group_by(Year, Sector) %>% 
+  summarise(effort = sum(NomActive, na.rm = TRUE)) %>% 
+  ungroup()%>% 
+  mutate(Version = "Old")
+
+effort_mapped_all<-effort_mapped_old %>% 
+  full_join(effort_mapped_new) 
+
+plot_effort_tot<-ggplot(effort_mapped_all, aes(x = Year, y = effort, color = Version, linetype = Sector))+
+  geom_line()+
+  labs(title = "Totla effort", x="Year",y="Nominal effort (NomActive)")+ 
+  # scale_y_continuous(limits = c(limMin, limMax))+ # not sure about this .... 
+  theme_bw()+
+  theme(axis.text=element_text(size=9),
+        axis.title=element_text(size=9),
+        title=element_text(size=8))
+plot_effort_tot
+
+pdf("Output/Summary_total_NewVsOldData.pdf", width = 10, height=7)
+plot_effort_tot
+dev.off()
+
+## WARNINIG - effort from original adn old files match but not from new file! 
+# back to original data (e.g. mapped_1950_I_100.csv) for more exploration... it is possibly the aggregation to SAUP, EEZ, LME and FAO where different polygons have been used. 
+# check that the sum of effort for 1 year, 1 sector, across all countries (SAUP I guess) from the original csv files matches the value in the new effort dataset 
+
+effort_file_list <- list.files(file.path(yannick_dir_new, "effort_mapped_bycountry"), pattern = ".csv", full.names = TRUE)
+industrial_effort_list <- effort_file_list[grep(pattern="1990", effort_file_list)]
+
+sum_value<-vector()
+for(i in 1:length(industrial_effort_list)){
+  this_csv <- data.table::fread(industrial_effort_list[[i]])
+  sum_value[[i]]<-sum(this_csv$NomActive, na.rm = TRUE)
+}
+effort_original<-sum(sum_value)
+
+# effort from old effort file
+effort_tot_old<-effort_old %>% 
+  filter(Year == 1990) 
+effort_tot_old<-sum(effort_tot_old$NomActive, na.rm = TRUE)
+
+# effort from new effort file 
+effort_tot_new<-effort_new %>% 
+  filter(Year == 1990) 
+effort_tot_new<-sum(effort_tot_new$NomActive, na.rm = TRUE)
+
+effort_original
+effort_tot_old
+effort_tot_new
+
+# OR
+filter(effort_mapped_all, Year == 1990) %>% group_by(Version) %>% summarise(sum(effort))
+
+# plot by LME
 # explore by LME - some LMEs 
 sort(unique(effort_new$LME))
 sort(unique(effort_new$fao_area))
@@ -41,14 +100,14 @@ effort_mapped_new<-effort_new %>%
   group_by(Year, LME, Sector) %>% 
   summarise(effort = sum(NomActive, na.rm = TRUE)) %>% 
   ungroup() %>% 
-  mutate(Version = "Old")
+  mutate(Version = "New")
 
 # cannot do comparison at FAO or EEZ becasue these are missing from old dataset - need to load another dataset 
 effort_mapped_old<-effort_old %>% 
   group_by(Year, LME, Sector) %>% 
   summarise(effort = sum(NomActive, na.rm = TRUE)) %>% 
   ungroup()%>% 
-  mutate(Version = "New")
+  mutate(Version = "Old")
 
 effort_mapped_all<-effort_mapped_old %>% 
   full_join(effort_mapped_new) 
@@ -69,7 +128,6 @@ for(i in 1:length(effort_list)){
   plot_effort[[i]]<-ggplot(effort_list[[i]], aes(x = Year, y = effort, color = Version, linetype = Sector))+
     geom_line()+
     labs(title = paste("LME",unique(effort_list[[i]]$LME),sep=" "), x="Year",y="Nominal effort (NomActive)")+ 
-    # scale_y_continuous(limits = c(limMin, limMax))+ # not sure about this .... 
     theme_bw()+
     theme(axis.text=element_text(size=9),
           axis.title=element_text(size=9),
@@ -89,15 +147,15 @@ for(i in 1:length(effort_list)){
 # print plots in pdf
 getwd()
 
-# # 4 plot per page - all effort 
+# 4 plot per page - all effort
 # install.packages("gridExtra")
 library("gridExtra")
-ggsave("Output/Summary_LME_NewVsOldData2.pdf", marrangeGrob(grobs = plot_effort, nrow=2, ncol=2), device = "pdf")
+ggsave("Output/Summary_LME_NewVsOldData.pdf", marrangeGrob(grobs = plot_effort, nrow=2, ncol=2), device = "pdf")
 # ggsave("EffortOld.pdf", marrangeGrob(grobs = plot_effort_old, nrow=2, ncol=2), device = "pdf")
 # ggsave("CatchOld.pdf", marrangeGrob(grobs = plot_catch_old, nrow=2, ncol=2), device = "pdf")
 
 # one page per LME
-pdf("Output/Summary_LME_NewVsOldData.pdf", width = 10, height=5)
+pdf("Output/Summary_LME_NewVsOldData2.pdf", width = 10, height=5)
 plot_effort
 dev.off()
 
@@ -115,10 +173,10 @@ effort_mapped_new_FG<-effort_new %>%
   ungroup() %>% 
   mutate(Version = "New")
 
-ggplot(effort_mapped_new_FG, aes(x = Year, y = effort, color = Version))+ 
-  geom_line()+
-  theme_bw()+
-  facet_wrap(~FGroup)
+# ggplot(effort_mapped_new_FG, aes(x = Year, y = effort, color = Version))+ 
+#   geom_line()+
+#   theme_bw()+
+#   facet_wrap(~FGroup)
 
 effort_mapped_old_FG<-effort_old %>% 
   filter(Sector == "I",LME %in% keep) %>%
@@ -127,10 +185,10 @@ effort_mapped_old_FG<-effort_old %>%
   ungroup() %>% 
   mutate(Version = "Old")
 
-ggplot(effort_mapped_old_FG, aes(x = Year, y = effort, color = Version))+ 
-  geom_line()+
-  theme_bw()+
-  facet_wrap(~FGroup)
+# ggplot(effort_mapped_old_FG, aes(x = Year, y = effort, color = Version))+ 
+#   geom_line()+
+#   theme_bw()+
+#   facet_wrap(~FGroup)
 
 effort_mapped_all_FG<- effort_mapped_old_FG %>% 
   full_join(effort_mapped_new_FG)
@@ -159,20 +217,53 @@ effort_mapped_new_FG %>% summarise(trial = sum(effort))
 effort_mapped_old_FG %>% group_by(Year) %>% summarise(trial = sum(effort)) # different values by year  
 effort_mapped_new_FG %>% group_by(Year) %>% summarise(trial = sum(effort))
 
-# back to original data (e.g. mapped_1950_I_100.csv) for more exploration... it is possibly the aggregation to SAUP, EEZ, LME and FAO where different polygons have been used. 
+### column names (see emails from Yannick and notes on desktop re links to draft paper)
 
-# ARRIVATA QUI
+# eez_country_name = The EEZ/high seas name in which the Effort/Catch
+# is occurring (Rich's note = which EEZ was used for the new aggregation?)
+# fao_area = A number code to the FAO area, 
+# as per FAO notation (Rich's note = which FAO was used for the new aggregation?)
+# LME = A number code to the LME, as per NOAA notation (Rich's note = which FAO was used for the new aggregation?)
+# SAUP = ?? "Fcountry": Fishing country, in ISO3 notation. Ex supranational entities (USSR, Yugoslavia) 
+# are disaggregated to their constituent countries. Serbian Fishing Effort included with Montenegro.
+# Gear = not here before 
+# FGroup = Functional Group of the target species
+# Sector = Fishing sector (artisanal/industrial), defined by the law of the country 
+# (varies by country) - needs to be updated to I, UP and APW
+# NomActive = ?? "NomEffTotal": Total nominal fishing effort (artisanal and industrial, including reported and IUU), 
+# by country and year. Unmapped, non gear specific. where Nominal (i.e. not including the technological creep). 
+# where Active (i.e., active vessels - from draft paper: reconstruction considers "the ratio of vessels in activity to the total capacity".
+# unit is Kw
+# EffActive = effective effort - i.e. including technological creep. 
+# From draft paper:Effective effort has been calculated using a 3.5% creep average per year. 
+# NV = "NumberVessels": Number of fishing vessels by country and year.
+# P = ??"EnginePowerkW": Total engine power equivalent of the fishing fleet, by country and year. 
+# The non-motorised fleet was given an engine power equivalent to half the engine power of small 
+# (powered) vessels of that country (approx 2kW/vessel).
+# GT = ?? gross tonnage
+# Year = Year (end of the year) when the Effort/Catch is occurring
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 # NOT SURE THE BELWO IS NEEDED OR IS PART OF THE REGIONAL VS GLOBAL EXPLORATION. MIGHT NEED TO MOVE THERE AND ACCORDING TO OLDER FILES SAVED IN LOCAL 
-
-
-
-
-
-
-
 
 # load effort unmapped
 # is teh pick for LME 42 present in the data before the mapping? kind of - but different trend with clear smoothing after the pick. ALos lots of smoothing pre mapping with all countries. 
