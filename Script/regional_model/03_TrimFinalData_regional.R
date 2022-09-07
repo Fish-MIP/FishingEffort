376e71e96ff6db295e6fe2ad96384e3f236d7a8d
camillanovaglio <camilla.novaglio@gmail.com>
2022-09-08 06:23:50 +1000

    working on allocation of effort and catch to regional model area and etrapolation of spinup

--- Script/regional_model/03_TrimFinalData_regional.R
+++ Script/regional_model/03_TrimFinalData_regional.R
@@ -8,22 +8,22 @@ library("gridExtra")
 library("reshape2")
 library("raster")
 
-# see 05 for comments - this is the regionla version 
+# see 05 for comments - this is the regional version 
 
+#### load file ----
 yannick_dir_new <- "/rd/gem/private/users/yannickr"
 
 effort_new<-read_csv(file.path(yannick_dir_new, "all_effort_aggregated_regional_models.csv"))
 head(effort_new)
 
+#### aggregate artisanal power and unpower ----
 # aggregate artisanal
 effort_new_Artisanal<-effort_new %>% 
   filter(Sector !="I") %>% 
   group_by(model_area_name, SAUP, Gear, FGroup, Year) %>% 
   summarise(NomActive = sum(NomActive, na.rm = TRUE),
-            # EffActive = sum(EffActive, na.rm = TRUE), # no need for EffActive
             NV = sum(NV, na.rm = TRUE),
-            P = sum(P, na.rm = TRUE)) %>% # no need for GT
-  # GT = sum(GT, na.rm = TRUE)) %>% 
+            P = sum(P, na.rm = TRUE)) %>% 
   ungroup() %>% 
   mutate(Sector = "Artisanal")
 
@@ -32,12 +32,10 @@ head(effort_new_Artisanal)
 # aggregate industrial 
 effort_new_Industrial<-effort_new %>% 
   filter(Sector == "I") %>% 
-  group_by(model_area_name, SAUP, Gear, FGroup, Year) %>% # delete fao_area
+  group_by(model_area_name, SAUP, Gear, FGroup, Year) %>% 
   summarise(NomActive = sum(NomActive, na.rm = TRUE),
-            # EffActive = sum(EffActive, na.rm = TRUE), # no need for EffActive
             NV = sum(NV, na.rm = TRUE),
-            P = sum(P, na.rm = TRUE)) %>% # no need for GT
-  # GT = sum(GT, na.rm = TRUE)) %>% 
+            P = sum(P, na.rm = TRUE)) %>% 
   ungroup() %>% 
   mutate(Sector = "Industrial")
 
@@ -52,13 +50,11 @@ head(effort_tot)
 nrow(effort_tot)
 nrow(effort_new_Artisanal)+nrow(effort_new_Industrial)
 
-# effort to calculate spin-up
+### effort data to calculate spin-up in 05 ----
 effort_spinup<-effort_tot %>% 
   filter(Year >= 1950, Year <= 2017) 
 
-# check - plots 
-head(effort_spinup)
-
+### checks (can skip adn see with new mask)----
 trial<-effort_spinup %>% 
   group_by(model_area_name, Year) %>% 
   summarise(NomActive = sum(NomActive))
@@ -68,10 +64,9 @@ plot<-ggplot(trial, aes(y = NomActive, x = Year))+
   geom_line()+
   facet_wrap(~model_area_name, scales = "free")
 
-# WARNING something wrong with the land... 
+# WARNING something wrong with the land on this mask... 
 
 # compare with LME scale effort - Mediterranean sea (LME 26) and north sea (LME 22) are comparable in terms of shape file. 
-# WARNING SE_Australia much bigger than what it should be for your model (it's the whole SESSF and possibly OK for Beth - LME 41, 42, 43)
 # WARNING - when added all together some shape files overlap 
 # (e.g. SE_Australia and Bass strait) and the overlapping cells are allocated to one model only (i.e. bass strait)
 pdf("Output/regional_models.pdf")
@@ -97,7 +92,7 @@ pdf("Output/global_effort_fromRegionaModelMerging.pdf")
 plot
 dev.off()
 
-# file created in 02 
+# file created in 02 global version
 effort_lme<-read_csv(file.path(yannick_dir_new, "all_effort_aggregated.csv"))
 
 trial<-effort_lme %>% 
@@ -113,6 +108,5 @@ pdf("Output/global_effort_fromLMEmerging.pdf")
 plot
 dev.off()
 
-# print data 
-
+##### print data ----
 write.csv(effort_spinup, "/rd/gem/private/users/yannickr/effort_histsoc_1950_2017_regional_model.csv") 
